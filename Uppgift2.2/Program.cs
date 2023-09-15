2cb60e45d183d32f7b634ff722137de93edcfe76
Keli06001 <keli06001@utb.vaxjo.se>
2023-09-15 12:59:03 +0200

    Add project files.

---
+++ Uppgift2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin mätt i meter?");
+            string meterText = Console.ReadLine();
+            int meter = int.Parse(meterText);
+            Console.WriteLine("Hur långt hoppade Alma mätt i meter?");
+            string meteText = Console.ReadLine();
+            int mete = int.Parse(meteText);
+            int skillnad = meter - mete;
+            Console.WriteLine("Elin hoppade " + skillnad + "m längre än Alma");
+
+        }
+    }
+}
